bda970abdbcd7ba0f8582873c3aa98814b54f294
srinivasapsit <47416519+srinivasapsit@users.noreply.github.com>
2021-03-22 18:29:27 +0530

    added div code

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -10,4 +10,7 @@ public class Calculator {
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
+	public static void div(int num1,int num2){
+	      System.out.println(num1/num2);
+	  }
 }
