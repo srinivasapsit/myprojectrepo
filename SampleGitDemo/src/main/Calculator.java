f7a4a22b79301d4da96138b1d467f3a4229a1206
Admin <Admin@DESKTOP-58RE5M4>
2021-03-22 18:09:41 +0530

    Second Commit

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -6,5 +6,7 @@ public class Calculator {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
-
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 }
