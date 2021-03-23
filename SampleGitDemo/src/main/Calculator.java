365ea785fc6430664dad657615f85fbcc75b1db9
Admin <Admin@DESKTOP-58RE5M4>
2021-03-23 09:52:07 +0530

    added square function in br1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -16,4 +16,7 @@ public class Calculator {
 	public static void prod(int num1,int num2){
 	      System.out.println(num1*num2);
 	  }
+	public static void square(int num1){
+	      System.out.println(num1*num1);
+	  }
 }
